6a041dde28a441f8b0999271bacafa692a7f5214
rwa <wangrandk@gmail.com>
2021-03-03 14:14:11 +0100

    modify download path, add missing dependancies in func.sh

--- func.sh
+++ func.sh
@@ -1,12 +1,16 @@
 gitinstall()
 {
-    git clone $1
-    cd $(basename $1)
+   # sudo apt-get install g++
+   # sudo apt-get install make
+   # git clone $1
+    git -C /usr/local/src  clone $1
+    cd /usr/local/src/$(basename $1)
+   # cd $(basename $1)
     ./configure
-    make
+    make 
     sudo make install
-    cd ..
-    rm -rf $(basename $1)
-    ls
-    pwd
+   # cd ..
+   # rm -rf $(basename $1)
+   # ls
+   # pwd
 }
