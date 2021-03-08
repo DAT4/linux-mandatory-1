c1f0daea0308eb4f5f57a48bd497998bd6d14f6b
martin <s195469@student.dtu.dk>
2021-03-08 13:17:07 +0100

    added password

--- main.sh
+++ main.sh
@@ -2,6 +2,8 @@
 
 whiptail --msgbox --title "Welcome" "Welcome to this installer program\n click ok to continue..." 25 80
 
+PASSWORD=$(whiptail --passwordbox "Get sudo with password" 8 39 --title "Sudo" 3>&1 1>&2 2>&3)
+
 setpermissions $PASSWORD
 
 CHOICE=$(
@@ -15,22 +17,9 @@ whiptail --title "Installation" --menu "This is not printed" 16 100 9 \
 )
 
 case $CHOICE in
-	"1)")
-        gitinstall $URL
-	;;
-	"2)")
-        installtar $PASSWORD
-	;;
-
-	"3)")
-        whiptail --msgbox --title "Install from deb" "This is not implemented..." 25 80
-    ;;
-
-	"4)")
-        installpkg $PASSWORD
-    ;;
-
-	"5)") 
-        exit
-    ;;
+	"1)") gitinstall $URL;;
+	"2)") installtar $PASSWORD;;
+	"3)") whiptail --msgbox --title "Install from deb" "This is not implemented..." 25 80;;
+	"4)") installpkg $PASSWORD;;
+	"5)") exit;;
 esac
